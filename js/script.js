0d221c7a9dc282068a5cf480ec9bf0185d4ca199
vspcolab <164621141+vspcolab@users.noreply.github.com>
2025-03-03 20:43:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+// Simple slider script
+document.addEventListener("DOMContentLoaded", function() {
+  let slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+  const slideInterval = setInterval(nextSlide, 5000); // change slide every 5 seconds
 
+  function nextSlide() {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }
+});
